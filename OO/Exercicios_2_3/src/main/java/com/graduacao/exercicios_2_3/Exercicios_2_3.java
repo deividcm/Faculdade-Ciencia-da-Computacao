79128381e60666d1cd20ef556eff303a88f423cd
Sr-Lobinho <168032801+Sr-Lobinho@users.noreply.github.com>
2026-03-16 10:16:01 -0300

    Menu dos exercícios 2.3 feito
    Falta fazer os exercícios

---
+++ OO/Exercicios_2_3/src/main/java/com/graduacao/exercicios_2_3/Exercicios_2_3.java
@@ -0,0 +1,44 @@
+
+package com.graduacao.exercicios_2_3;
+
+import java.util.Scanner;
+
+
+public class Exercicios_2_3 {
+    
+     public static int imprimeMenu(){
+        Scanner ler = new Scanner(System.in);
+        System.out.println("--------Exercicios2.3--------");
+        System.out.println("1 - Exercicio 01");
+        System.out.println("2 - Exercicio 02");
+        System.out.println("3 - Exercicio 03");
+        System.out.println("0 - Sair");
+        System.out.println("-----------------------------");
+        System.out.println("Escolha uma opcao:");
+        
+        return ler.nextInt();
+    }
+    
+    public static void main(String[] args) {
+        int opcao = 0;
+        
+        do{
+            opcao = imprimeMenu();
+            switch (opcao){
+                case 1 -> {
+                    System.out.println("Placeholder Exercicio 01");
+                }
+                case 2 -> {
+                    System.out.println("Placeholder Exercicio 02");
+                }
+                case 3 -> {
+                    System.out.println("Placeholder Exercicio 03");
+                }
+            }
+            
+            
+            
+        }while(opcao !=0);
+        
+    }
+}
