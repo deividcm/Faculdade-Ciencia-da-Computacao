627d8314cc824cc6835d231955624d053612fcc4
SrLobinho <deivid.docarmomagalhaes@gmail.com>
2026-04-03 13:27:50 -0300

    Classe Quadrado criada pelo Isaque

--- OO/Roteiro01/src/main/java/com/graduacao/roteiro01/classes/Quadrado.java
+++ OO/Roteiro01/src/main/java/com/graduacao/roteiro01/classes/Quadrado.java
@@ -1,6 +1,50 @@
 package com.graduacao.roteiro01.classes;
 
-
-public class Quadrado {
+import java.util.Arrays;
     
+public class Quadrado {
+
+    Ponto p1, p2, p3, p4;
+
+    public Quadrado(Ponto p1, Ponto p2, Ponto p3, Ponto p4) {
+        this.p1 = p1;
+        this.p2 = p2;
+        this.p3 = p3;
+        this.p4 = p4;
+    }
+
+    // fórmula correta da distância
+    private double distancia(Ponto a, Ponto b) {
+        return Math.sqrt(Math.pow(a.getX() - b.getX(), 2) + Math.pow(a.getY() - b.getY(), 2));
+    }
+
+    public boolean ehQuadrado() {
+
+        double[] d = new double[6];
+
+        d[0] = distancia(p1, p2);
+        d[1] = distancia(p1, p3);
+        d[2] = distancia(p1, p4);
+        d[3] = distancia(p2, p3);
+        d[4] = distancia(p2, p4);
+        d[5] = distancia(p3, p4);
+
+        Arrays.sort(d);
+
+        return d[0] > 0 &&
+               d[0] == d[1] &&
+               d[1] == d[2] &&
+               d[2] == d[3] &&
+               d[4] == d[5];
+    }
+
+    public double area() {
+        double lado = distancia(p1, p2); 
+        return lado * lado;
+    }
+
+    public double perimetro() {
+        double lado = distancia(p1, p2);
+        return 4 * lado;
+    }
 }
